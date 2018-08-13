318c5c10ddc9bd9536d6aaf19af548df7391f280
038235 <gengb@jzteyao.com>
2018-08-13 17:51:36 +0800

    acerTest

--- src/com/Clone/Thing.java
+++ src/com/Clone/Thing.java
@@ -3,16 +3,18 @@ package com.Clone;
 import java.util.ArrayList;
 
 public class Thing implements Cloneable {
-    private ArrayList<String> arrayList = new ArrayList<String>();
-
+    private ArrayList arrayList = new ArrayList();
     @Override
     public Thing clone() {
         Thing thing = null;
         try {
             thing = (Thing) super.clone();
+            //深拷贝
+            thing.arrayList = (ArrayList<String>) this.arrayList.clone();
         } catch (CloneNotSupportedException ex) {
             ex.printStackTrace();
         }
+        return thing;
     }
 
     public void setValue(String value) {
--- src/com/youmeek/HelloWorld.java
+++ src/com/youmeek/HelloWorld.java
@@ -1,7 +1,13 @@
 package com.youmeek;
 
+import com.Clone.Thing;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("HelloWorld!!");
+        Thing thing = new Thing();
+        thing.setValue("张三");
+        Thing cloneThing = thing.clone();
+        cloneThing.setValue("李四");
+        System.out.println(thing.getValue());
     }
 }
