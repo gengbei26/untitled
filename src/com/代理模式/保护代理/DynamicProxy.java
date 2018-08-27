1e979f8a1a48c048036b4d961f7f917ba66a0e54
gengbei26 <gengbei26@163.com>
2018-08-27 11:15:01 +0800

    动态代理

---
+++ src/com/代理模式/保护代理/DynamicProxy.java
@@ -0,0 +1,38 @@
+package com.代理模式.保护代理;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * @ClassName DynamicProxy
+ * @Description 动态代理
+ * @Author deus
+ * @Data 2018/8/27 10:19
+ * @Version 1.0
+ **/
+public class DynamicProxy implements InvocationHandler {
+    private OrderApi order = null;
+    public OrderApi getProxyInterface(Order order){
+        this.order = order;
+        OrderApi orderApi = (OrderApi) Proxy.newProxyInstance(
+                order.getClass().getClassLoader(),
+                order.getClass().getInterfaces(),
+                this);
+        return orderApi;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable{
+        if(method.getName().startsWith("set")){
+            if(order.getOrderUser() != null
+            && order.getOrderUser().equals(args[1])){
+                return method.invoke(order,args);
+            }else{
+                System.out.println("对不起，" + args[1] + "您无权修改订单中的数据");
+            }
+        } else{
+            return method.invoke(order,args);
+        }
+        return null;
+    }
+}
---
+++ src/com/代理模式/保护代理/ProxyClient.java
@@ -0,0 +1,36 @@
+package com.代理模式.保护代理;
+
+/**
+ * @ClassName ProxyClient
+ * @Description TODO
+ * @Author deus
+ * @Data 2018/8/24 17:25
+ * @Version 1.0
+ **/
+public class ProxyClient {
+    public static void main(String[] args) {
+        //保护代理
+//        OrderApi orderApi = new OrderProxy(new Order(
+//                "设计模式",
+//                100,
+//                "张三"));
+        //////////////////////////////////////////////
+
+        //动态代理
+        Order order = new Order(
+                "设计模式",
+                100,
+                "张三");
+        DynamicProxy dynamicProxy = new DynamicProxy();
+        OrderApi orderApi = dynamicProxy.getProxyInterface(order);
+        ////////////////////////////////////////////////////////////
+
+        //测试
+        orderApi.setOrderNum(333, "李四");
+        System.out.println("李四修改后，没有变化：" + orderApi.getOrderNum());
+        orderApi.setOrderNum(555, "张三");
+        System.out.println("张三修改后，变化了：" + orderApi.getOrderNum());
+
+
+    }
+}
