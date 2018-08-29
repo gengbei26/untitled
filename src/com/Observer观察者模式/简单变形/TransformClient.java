8a5c91c480ef934b14a2761793276f0eae47dba2
gengbei26 <gengbei26@163.com>
2018-08-29 17:59:26 +0800

    观察者模式

--- src/com/Observer观察者模式/简单变形/TransformClient.java
+++ src/com/Observer观察者模式/简单变形/TransformClient.java
@@ -8,6 +8,10 @@ package com.Observer观察者模式.简单变形;
  * @Version 1.0
  **/
 public class TransformClient {
+    //观察者模式的简单变形使用，按水质级别发送消息给不同的观察者
+    //一种方案是目标通知，但观察者按水质级别判断是否做操作
+    //另一种方案是在目标里判断，只通知需要通知的观察者
+    //一般推荐选择第二种，有利于业务控制和扩展
     public static void main(String[] args) {
         //创建水质主题对象
         WaterQuality subject = new WaterQuality();
